d6b300422ee28a9ed32e21f02a76ed44eb7b92ac
koshkatze <kffmccarthy@gmail.com>
2023-07-29 00:31:11 +0100

    adding pixel art generator

---
+++ editor/script.js
@@ -0,0 +1,69 @@
+const content = document.getElementById("content");
+
+const newGrid = () => [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+];
+
+const grid = newGrid();
+
+const copyButton = document.getElementById("copy");
+
+copyButton.addEventListener("click", (event) => {
+    navigator.clipboard.writeText(grid.toString());
+})
+
+const drawGrid = (grid) => {
+    for (let rowIndex = 0; rowIndex < grid.length; rowIndex++) {
+        const row = grid[rowIndex];
+        const rowElement = addRow();
+        for (let cellIndex = 0; cellIndex < row.length; cellIndex++){
+            const cell = row[cellIndex];
+            addCell(rowElement, rowIndex, cellIndex, cell)
+        }
+    }
+}
+
+const addRow = () => {
+    const element = document.createElement("div");
+    element.setAttribute("class", "row");
+    content.appendChild(element);
+    return element;
+};
+
+const addCell = (rowElement, rowIndex, cellIndex, initialValue) => {
+    const element = document.createElement("div");
+    element.setAttribute("class", "cell");
+    element.innerText = cellValueToString(initialValue);
+    element.addEventListener("click", (event) => {
+        const currentCellValue = grid[rowIndex][cellIndex];
+        const newCellValue = currentCellValue === 0 ? 1 : 0;
+        grid[rowIndex][cellIndex] = newCellValue;
+        event.target.innerText = cellValueToString(newCellValue);
+    })
+    rowElement.appendChild(element);
+}
+
+const cellValueToString = (cellValue) => {
+    if (cellValue === 0) {
+        return "⬜";
+    }
+    return "⬛";
+}
+
+drawGrid(grid)
+
